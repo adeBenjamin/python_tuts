af6e06cb521f8286245b0787ab98a83c4d1fdb64
adeBenjamin <ab@adebenjamin.com>
2019-02-25 16:50:48 +0000

    added a for loop: need to examine on the splitlines() method

--- ex-code/cat_fruits_to_cli.py
+++ ex-code/cat_fruits_to_cli.py
@@ -1,5 +1,13 @@
 content_file = open("fruits.txt")
-fruits = content_file.read()
+fruits =  content_file.read()
 content_file.close()
 
-print(fruits)
+# print(fruits)
+
+# FOR LOOP
+# first convert content into lists
+
+fruits_list = fruits.splitlines()
+
+for each_fruit in fruits_list:
+    print(each_fruit + " is a fruit")
